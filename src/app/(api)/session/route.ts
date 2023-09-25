8acf1ca6080d4f52b2f0e9c9dae1fec1504228c9
Nnamdi Ogbu <ogbunnamdi@gmail.com>
2023-09-25 10:39:23 -0400

    hide .env

--- src/app/(api)/session/route.ts
+++ src/app/(api)/session/route.ts
@@ -1,19 +1,23 @@
 import { createSqlChain } from "@/app/_sqlchain";
 import { queryFromCache } from "@/app/_storage/cache";
-import { BufferMemory } from "langchain/memory";
+
+interface SessionInit {
+  openAIKey: string;
+  databaseUrl: string;
+}
 
 export async function POST(request: Request) {
-  // try {
-  //   const payload = await request.json();
-  //   const { connectStr }: { connectStr: string } = payload;
-  //   if (connectStr === undefined) throw new Error("Connection string not supplied");
-  //   const memory = await queryFromCache("memory", getChatMemory);
-  //   if (!memory) throw new Error("Cannot process this request at this time, try again later");
-  //   const chain = await createSqlChain();
-  //   chain.memory = memory;
-  //   return new Response("Works", { status: 200 });
-  // } catch (err) {
-  //   console.error(err);
-  //   new Response(JSON.stringify(err), { status: 500 });
-  // }
+  try {
+    const payload = (await request.json()) as SessionInit;
+    const { openAIKey, databaseUrl } = payload;
+    if (connectStr === undefined) throw new Error("Connection string not supplied");
+    const memory = await queryFromCache("memory", getChatMemory);
+    if (!memory) throw new Error("Cannot process this request at this time, try again later");
+    const chain = await createSqlChain();
+    chain.memory = memory;
+    return new Response("Works", { status: 200 });
+  } catch (err) {
+    console.error(err);
+    new Response(JSON.stringify(err), { status: 500 });
+  }
 }
